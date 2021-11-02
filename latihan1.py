92efde9f9c9c6e1ab11036cb31bb2342801749ca
akmal <akmal@pelitabangsa.ac.id>
2021-11-02 12:21:04 +0700

    menambahkan file readme

--- latihan1.py
+++ latihan1.py
@@ -1,10 +1,3 @@
-# materi1
-## latihan membuat program sederhana 
-### berikut programnnya
-kita menggunakan pycram untuk editornnya
-
-1.latihan1'<p>'
-codingannya
 #Kondisi if adalah kondisi yang akan di eksisusi oleh program jika bernilai benar atau TRUE
 
 nilai = 9
@@ -16,12 +9,3 @@ if(nilai > 7):
 #jika kondisi salah/FALSE maka program tidak akan mengeksekusi perintah dibawanya
 if(nilai > 10):
     print("sembilan lebih besar dari angka sepuluh") # kondisi salah, maka tidak tereksekusi
- Ini contoh codingannya'<p>'
- ![Gambar 1](screenshoot/s1.JPG)
-
- Berikut hasil run scripnnya
- "C:\belajar python\venv\Scripts\python.exe" C:/materi/materi1/latihan1.py
-sembilan lebih besar dari angka tujuh
-
-Process finished with exit code 0'<p>'
-
